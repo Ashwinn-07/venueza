89c6e505eef28a3fc486811baa743e2f0a79a937
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-03-31 13:49:39 +0530

    message model created

---
+++ backend/src/models/message.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IMessage extends Document {
+  sender: mongoose.Types.ObjectId;
+  senderModel: string;
+  receiver: mongoose.Types.ObjectId;
+  receiverModel: string;
+  content: string;
+  createdAt: Date;
+}
+
+const messageSchema: Schema = new Schema(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "senderModel",
+    },
+    senderModel: { type: String, required: true, enum: ["User", "Vendor"] },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "receiverModel",
+    },
+    receiverModel: { type: String, required: true, enum: ["User", "Vendor"] },
+    content: { type: String, required: true },
+  },
+  { timestamps: { createdAt: true, updatedAt: false } }
+);
+
+const Message = mongoose.model<IMessage>("Message", messageSchema);
+export default Message;
